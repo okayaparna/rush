c9b64f8e43d176c3925d520ed1a762a9ee8a07b5
okayaparna <krisa304@newschool.edu>
2023-04-28 11:40:19 -0400

    lat-long rounded to 5

--- wya.js
+++ wya.js
@@ -1,17 +1,21 @@
-// Get the latitude and longitude of the user's location
+// Get the latitude and longitude of the user's location and round off to 5 digits
 function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      alert("Geolocation is not supported by this browser.");
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const lat = position.coords.latitude.toFixed(5);
+      const long = position.coords.longitude.toFixed(5);
+      showPosition(lat, long);
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
   }
-  
-  // Update the latitude and longitude in the DOM
-  function showPosition(position) {
-    document.querySelector('.lat-number').textContent = position.coords.latitude;
-    document.querySelector('.long-number').textContent = position.coords.longitude;
-  }
-  
-  // Attach the click event listener to the button
-  document.querySelector('#station-go').addEventListener('click', getLocation);
+}
+
+// Update the latitude and longitude in the DOM
+function showPosition(lat, long) {
+  document.querySelector('.lat-number').textContent = lat;
+  document.querySelector('.long-number').textContent = long;
+}
+
+// Attach the click event listener to the button
+document.querySelector('#station-go').addEventListener('click', getLocation);
