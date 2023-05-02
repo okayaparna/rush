9860c314e733f431a75a02af151a3c46aab8dfb5
okayaparna <krisa304@newschool.edu>
2023-05-02 12:03:22 -0400

    Update wya-test.js

--- wya-test.js
+++ wya-test.js
@@ -1,15 +1,21 @@
-const stationGoBtn = document.getElementById('station-go');
-const nameInput = document.getElementById('name');
-const stationModelDiv = document.getElementById('station-model');
-
-stationGoBtn.addEventListener('click', () => {
-  const stationName = nameInput.value.toLowerCase();
-  const stationModel = document.querySelector(`.${stationName}-model`);
-  const fillerModel = document.querySelector('.filler-station-model');
-
-  if (stationModel) {
-    stationModelDiv.innerHTML = stationModel.innerHTML;
-  } else {
-    stationModelDiv.innerHTML = fillerModel.innerHTML;
+// Get the latitude and longitude of the user's location and round off to 5 digits
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const lat = position.coords.latitude.toFixed(5);
+        const long = position.coords.longitude.toFixed(5);
+        showPosition(lat, long);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
   }
-});
+  
+  // Update the latitude and longitude in the DOM
+  function showPosition(lat, long) {
+    document.querySelector('.lat-number').textContent = lat;
+    document.querySelector('.long-number').textContent = long;
+  }
+  
+  // Attach the click event listener to the button
+  document.querySelector('#station-go').addEventListener('click', getLocation);
