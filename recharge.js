8f64204f17030cf955a5e98c9fea9d09fe596d60
okayaparna <krisa304@newschool.edu>
2023-05-05 14:40:40 -0400

    added splash screen

--- recharge.js
+++ recharge.js
@@ -58,11 +58,55 @@ function mouseUpHandler() {
   document.removeEventListener('mouseup', mouseUpHandler);
 }
 
-  
-  
-  
-  
-  
+// splash-screen
+
+window.addEventListener('load', function() {
+  const splashScreen = document.querySelector('.splash-screen');
+  splashScreen.style.opacity = 0;
+  let opacity = 0;
+  const fadeInTimerId = setInterval(function() {
+    if (opacity >= 1) {
+      clearInterval(fadeInTimerId);
+      setTimeout(function() {
+        const fadeOutTimerId = setInterval(function() {
+          if (opacity <= 0) {
+            clearInterval(fadeOutTimerId);
+            splashScreen.style.display = 'none';
+            // Unhide the rest of the content
+            const content = document.querySelector('.content');
+            content.style.display = 'block';
+          }
+          splashScreen.style.opacity = opacity;
+          opacity -= 0.01;
+        }, 20); // Update the opacity every 20 milliseconds (for a smoother animation)
+      }, 3000); // Wait for 2 seconds before fading out
+      return;
+    }
+    splashScreen.style.opacity = opacity;
+    opacity += 0.01;
+  }, 10); // Update the opacity every 10 milliseconds (for a smoother animation)
+});
+
+// fade in and out and body appears
+const splashScreen = document.querySelector('.splash-screen');
+const content = document.querySelector('.content');
+
+// Fade in splash screen
+splashScreen.style.opacity = 0;
+splashScreen.style.transition = 'opacity 1s';
+setTimeout(() => {
+  splashScreen.style.opacity = 1;
+}, 0);
+
+// Fade out splash screen and remove from DOM
+setTimeout(() => {
+  splashScreen.style.opacity = 0;
+  setTimeout(() => {
+    splashScreen.parentNode.removeChild(splashScreen);
+  }, 500); // wait for 0.5 seconds for fade out animation to finish
+  content.classList.remove('hidden');
+}, 2000);
+
   
   
   
